6e3477300c6f499327cb99f4d7e88a8609a32fee
Brandon <Blent1050@yahoo.com>
2019-01-24 19:03:43 -0500

    changed pot

--- server.js
+++ server.js
@@ -74,6 +74,6 @@ app.post('/api/movies', (req, res) => {
   res.status(201).json(movies);
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log('Server listening on port 5000');
 });
