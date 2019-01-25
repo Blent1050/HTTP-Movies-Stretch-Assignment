f9e4b39a9a6e704d0a02c56c4cde59a023b4abcb
Brandon <Blent1050@yahoo.com>
2019-01-24 19:44:46 -0500

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const CORS = require('cors');
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -74,6 +74,17 @@ app.post('/api/movies', (req, res) => {
   res.status(201).json(movies);
 });
 
-app.listen(5000, () => {
-  console.log('Server listening on port 5000');
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('friends/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log('server listening on port 5000');
 });
